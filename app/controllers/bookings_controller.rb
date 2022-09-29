ab727467e3656a15af512c94693f754a04298f17
zuwarka <soap.sony@gmail.com>
2022-09-29 15:52:55 +0300

    add models with fields; add migrations for them; add empty controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+end
---
+++ app/models/booking.rb
@@ -0,0 +1,14 @@
+class Booking < ApplicationRecord
+  has_many :rooms
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 30 }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX }
+  before_save { self.email = email.downcase }
+end
