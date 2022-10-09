19472a7a7a03f9b303e51ef2468814a1f56591c1
zuwarka <soap.sony@gmail.com>
2022-10-09 12:35:23 +0300

    add currency api; there is not checking errors

--- Gemfile
+++ Gemfile
@@ -58,6 +58,10 @@ gem 'bootstrap-will_paginate', '1.0.0'
 
 gem 'image_processing', '~> 1.2'
 
+gem "sidekiq", "~> 6.5"
+
+gem "csv"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
@@ -75,5 +79,3 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
-
-gem "sidekiq", "~> 6.5"
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
   def index
-    @rooms = Room.paginate(page: params[:page], per_page: 2)
+    #@rooms = Room.paginate(page: params[:page], per_page: 2)
+    #currency_api_check_error
   end
 
   def about
 
+  end
+
+  private
+  #@path != 'daily_json' && @path != 'latest' &&
+  def currency_api_check_error
+    @path = params[:query_path]
+    if @path.blank? || (@path != 'daily_jsonp')
+      render json: { error: true, message: "Invalid query_path" }, status: :bad_request
+    end
   end
 end
