8b9bf375cdd8cf2306bdab310c04f09d7a54fe01
zuwarka <soap.sony@gmail.com>
2022-10-14 11:35:01 +0300

    add some new rspecs

---
+++ spec/jobs/export_bookings_job_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'csv'
+
+RSpec.describe ExportBookingsJob, type: :job do
+  describe '#perform_later' do
+    before { ActiveJob::Base.queue_adapter = :test }
+
+    it 'will be enqueued' do
+      expect do
+        ExportBookingsJob.perform_later(1)
+      end.to have_enqueued_job
+    end
+
+    it 'will generate log files' do
+      begin
+        ExportBookingsJob.perform_now
+        expect(File.exist?('confirmed_bookings.csv')).to eq(true)
+        expect(File.exist?('confirmed_bookings.xlsx')).to eq(true)
+        rescue ArgumentError # very bad but i don't what to do here :(
+      end
+    end
+  end
+end
---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Booking, type: :model do
+end
